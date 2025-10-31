80ac753321a1f35d382cbede3959b083fbbeac0f
이정진 <lsch4435@gmail.com>
2025-10-31 05:21:36 +0000

    Revert "Fix: left menu floating and portfolio click issue"
    This reverts commit de4e7b3e38e5d52c16cfdcba320cab1c6563f470.

--- js/main.js
+++ js/main.js
@@ -194,10 +194,3 @@ window.onload = function () {
 }
 
   
-document.querySelectorAll('.nav a').forEach(link => {
-  link.addEventListener('click', function() {
-    document.querySelectorAll('.nav a').forEach(l => l.classList.remove('active'));
-    this.classList.add('active');
-  });
-});
-
--- js/menuFix.js
+++
@@ -1,7 +0,0 @@
-
-const navToggler = document.querySelector('.nav-toggler');
-const aside = document.querySelector('.aside');
-navToggler.addEventListener('click', () => {
-  aside.classList.toggle('active');
-});
-
